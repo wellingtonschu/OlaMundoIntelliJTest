fb09b1929f469fb488f1688b6e9387e9ec1503d4
Wellington <wellington.schu@live.com>
2016-02-17 10:59:03 -0200

    Initial commit

---
+++ src/OlaMundo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class OlaMundo
+{
+    public static void main (String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Olá Mundo");
+
+        input.close();
+    }
+}
